0a4e289d4b2fb12518b80498226e197e9a239efe
parthmittal <parth15069@iiitd.ac.in>
2015-10-11 06:44:07 +0530

    more login

--- taskzilla/views.py
+++ taskzilla/views.py
@@ -4,12 +4,12 @@ from .models import Task
 
 def index(request):
 	tasks_list = Task.objects.filter(closed=False)
-	context = {'tasks_list' : tasks_list,}	
+	context = {'tasks_list' : tasks_list, 'user' : request.user,}
 	return render(request, 'taskzilla/index.html', context)
 
 def task_page(request, task_id):
 	task = Task.objects.get(pk=task_id)
-	context = {'task' : task, 'comments': task.comment_set.all()}
+	context = {'task' : task, 'comments': task.comment_set.all(), 'user' : request.user}
 	return render(request, 'taskzilla/task_page.html', context)
 
 def login_page(request):
