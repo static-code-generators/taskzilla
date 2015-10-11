5f6ce53504182fe082933ee2722cee1629ef7298
Jai Luthra <me@jailuthra.in>
2015-10-11 06:27:19 +0530

    Views: added login page

--- taskzilla/urls.py
+++ taskzilla/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^tasks/(?P<task_id>[0-9]+)/$', views.task_page, name='task_page'),
+	url(r'^login/$', views.login_page, name='login_page'),
 ]
--- taskzilla/views.py
+++ taskzilla/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 from .models import Task
 
 def index(request):
@@ -11,3 +11,14 @@ def task_page(request, task_id):
 	task = Task.objects.get(pk=task_id)
 	context = {'task' : task, 'comments': task.comment_set.all()}
 	return render(request, 'taskzilla/task_page.html', context)
+
+def login_page(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		print(user, username, password)
+		if user is not None:
+			login(request, user)
+			return render(request, 'taskzilla/login.html', {'loggedin' : True})
+	return render(request, 'taskzilla/login.html', {'loggedin': False})
